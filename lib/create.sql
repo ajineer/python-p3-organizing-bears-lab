5bc6db7b926511f91ebfa6d92c88ff398470f449
Mark <ajineer@outlook.com>
2023-07-27 09:59:44 -0600

    finished lab

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+        
+        id INT,
+        name TEXT,
+        age INT,
+        sex TEXT,
+        color TEXT,
+        temperament TEXT,
+        alive BOOLEAN
+        );
---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears 
+(
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive) 
+VALUES (
+    "Mr. Chocolate", 
+    3, 
+    "M", 
+    "brown", 
+    "FAAFO", 
+    true);
+    
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "F", "brown", "FAAFO", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "M", "black", "FAAFO", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 9, "M", "brown", "FAAFO", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "brown", "FAAFO", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "F", "black", "FAAFO", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "M", "red", "FAAFO", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
+
